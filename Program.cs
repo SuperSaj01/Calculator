625b73a5286a59de10e4793eacc0743e3126ccb6
SuperSaj01 <89708281+SuperSaj01@users.noreply.github.com>
2021-10-07 23:34:45 +0100

    The making of THE BEST CALCULATOR
    the start

--- Program.cs
+++ Program.cs
@@ -6,49 +6,13 @@ namespace My_First_Program
     {
         static void Main(string[] args)
         {
-            Console.Title = "Calculator";
-            double num01, num02;
+            int a = 5;
+            int b = 1;
 
-            Console.WriteLine("press y to start a new calculation, press n to exit");
-            // what is going on?
-            while(Console.ReadLine() == "y" )
-            {
-            Console.Write("Please input a number: ");
+            Console.Write("{0}{1}", a, b);
 
-            num01 = Convert.ToDouble (Console.ReadLine());
+            Console.ReadLine();
 
-            Console.Write("Please input another number: ");
-
-            num02 = Convert.ToDouble (Console.ReadLine());
-
-            Console.WriteLine("please input what method you would like:\n Multiplication(*)\n Division(/)\n Add(+)\n Subtact(-)");
-        
-            string method = Console.ReadLine();
-            if(method == "*")
-            {
-                Console.WriteLine(num01 * num02);
-                Console.WriteLine("'y' to carry on 'n' to exit");
-            }
-            if(method == "/")
-            {
-                Console.WriteLine(num01 / num02);
-                Console.WriteLine("'y' to carry on 'n' to exit");
-            }
-
-            if(method == "+")
-            {
-                Console.WriteLine(num01 + num02);
-            }
-
-            if(method == "-")
-            {
-                Console.WriteLine(num01 - num02);
-            }
-            //exit
-            
-
-            }
-          
         }
     }
 }
